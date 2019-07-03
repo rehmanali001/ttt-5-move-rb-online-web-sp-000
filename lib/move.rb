c2b6a7ebc2e2155048de8391a8d558cdcb259e12
Rehman Ali <rehmanali001@gmail.com>
2019-07-03 22:17:56 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,9 +8,3 @@ end
 
 
 # code your input_to_index and move method here!
-
-def input_to_index(num)
-  
-end
-
-num = ['1'.to_i, '2'.to_i, '3'.to_i, '4'.to_i, '5'.to_i, '6'.to_i,'7'.to_i, '8'.to_i, '9'.to_i]
